d6eedd987564f89f8039f1f6ea311b52b532022a
Ashley <ashuhleex03@outlook.com>
2026-09-17 13:25:17 -0400

    feat: implement lazy media loading and asset prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ashuhlee.ca',
-  integrations: [react()]
+  integrations: [react()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
 });
